2077fc9b3dcd1fa70e4c47e70ca5786b5b315ced
Paulo <paulotcj@gmail.com>
2021-08-20 19:38:55 -0400

    .

---
+++ BenchmarkerApp/BenchmarkerApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+using System.Text;
+
+namespace BenchmarkerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var results = BenchmarkRunner.Run<Demo>();
+        }
+    }
+
+    [SimpleJob(RuntimeMoniker.Net50, baseline:true)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [SimpleJob(RuntimeMoniker.Net472)]
+    [MemoryDiagnoser]
+    public class Demo
+    {
+        [Benchmark(Baseline =true)]
+        public string GetFullStringNormally()
+        {
+            string output = "";
+            for (int i = 0; i < 100; i++)
+            {
+                output += i;
+            }
+
+            return output;
+        }
+
+
+        [Benchmark]
+        public string GetFullStringNormally2()
+        {
+            var output = new StringBuilder();
+            for (int i = 0; i < 100; i++)
+            {
+                output.Append(i);
+            }
+
+            return output.ToString();
+        }
+    }
+}
